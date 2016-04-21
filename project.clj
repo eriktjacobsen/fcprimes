b081aefdf1969d8c4a9450a94e72f9e20d74b0d4
Erik Jacobsen <erik.t.jacobsen@gmail.com>
2016-04-20 22:03:36 -0700

    Skeleton Design

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/eriktjacobsen/fcprimes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main fcprimes.core
   :dependencies [[org.clojure/clojure "1.7.0"]])
--- src/fcprimes/core.clj
+++ src/fcprimes/core.clj
@@ -1,6 +1,26 @@
-(ns fcprimes.core)
+(ns fcprimes.core
+  (:require [clojure.string :as str])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+; Starting with n, perhaps lazy in future
+; I believe filtering from a range is more performant than generating on the fly -v2
+(defn primes
+  "Generates a list of primes, from 2 to n"
+  [n]
+  (range n))
+
+(defn times-table
+  "Takes list of numbers, generates a multiplication table"
+  [numbers]
+  numbers)
+
+(defn print-table
+  "Outputs a multiplcation table to the screen"
+  [table]
+  (println table))
+
+(defn -main [& args]
+  (-> (primes 10)
+    (times-table)
+    (print-table)))
